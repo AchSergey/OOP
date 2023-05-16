d218a6cef6fc51e1b11fcd6666e8a75ea41847f1
AchSergey <112536624+AchSergey@users.noreply.github.com>
2023-05-16 05:55:41 +0300

    Create Flat.java

---
+++ Flat.java
@@ -0,0 +1,21 @@
+package ru.inno.course.oop;
+
+public class Flat {
+
+    String address;
+    int entrance;
+    int floor;
+    int numberOfRooms;
+
+
+    public Flat(String newaddress, int newentrance, int newfloor,  int newnumberOfRooms, String color ) {
+         address = newaddress;
+         entrance = newentrance;
+         floor = newfloor;
+         numberOfRooms = newnumberOfRooms;
+
+
+
+
+    }
+}
