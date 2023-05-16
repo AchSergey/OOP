dc26693c002d90e0d6df56423240dc9b5fb57054
AchSergey <112536624+AchSergey@users.noreply.github.com>
2023-05-16 05:54:50 +0300

    Create Item.java

---
+++ Item.java
@@ -0,0 +1,21 @@
+ int articleNumber;
+    double price;
+    int quantity;
+    String color;
+
+    public Item(  String name,  int articleNumber,  double price,  int quantity,  String color) {
+        this.name = name;
+        this.articleNumber = articleNumber;
+        this.price = price;
+        this.quantity = quantity;
+        this.color = color;
+
+    }
+
+}
+
+
+//- название
+// - артикул
+// - цена
+// - количество.
